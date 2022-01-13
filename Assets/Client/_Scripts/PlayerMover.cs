a99d143d84ab30fb0d6d61c86d37b603d0d0e84b
Minilay <minilie88@gmail.com>
2022-01-13 22:25:38 +0600

    Basic Input And Colors
    TO SQUASH

---
+++ Assets/Client/_Scripts/PlayerMover.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(
+            new Vector3(0,
+                Input.GetAxisRaw("Horizontal") * _rotationSpeed * Time.deltaTime,
+                0
+                ));
+    }
+}
